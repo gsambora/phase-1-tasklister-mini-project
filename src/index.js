be451a740a7389b0772b6e87b51d43ba8984e9cb
Hana <grace.sambora@gmail.com>
2023-05-21 00:47:22 -0500

    Complete challenge

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  let form = document.querySelector('form');
+  let newInput = document.getElementById('new-task-description');
+
+  document.querySelector('form').addEventListener('submit', (e)=> {
+    e.preventDefault();
+    console.log("Button was clicked!");
+    handleToDo(newInput.value);
+
+    form.reset();
+    
+  })
 });
+
+function handleToDo(todo){
+  //console.log("new todo: ", todo);
+  let p = document.createElement('li');
+  p.textContent = todo;
+  document.querySelector('#tasks').appendChild(p);
+}
